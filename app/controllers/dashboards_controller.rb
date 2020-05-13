bfea97e6958c5286797f49b496f7cd2c425a0c86
codeloopy <codeloopy@gmail.com>
2020-05-12 23:38:00 -0400

    refactored the dash partial, started to layout the admin panel

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,17 +3,25 @@ class DashboardsController < ApplicationController
   before_action :only_admin_dash, only: :admindash
   before_action :find_user, only: [:userdash, :admindash]
 
+  before_action :find_all_users, only: [:userdash, :admindash]
+
   def userdash
     @kites = Kite.where(user_id: current_user)
   end
 
   def admindash
+    @kites = Kite.all
   end
 
   private
 
     def find_user
       @user = current_user
+    end
+
+    def find_all_users
+      @users = User.all
+
     end
 
     def only_admin_dash
