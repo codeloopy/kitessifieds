34fafd8a0528ba93023f94a0ccd8baa08dabadc8
codeloopy <codeloopy@gmail.com>
2020-05-22 21:25:00 -0400

    setup Devise confirmable and sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,9 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  # config.active_storage.service = :local
+  # Active storage - S3
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -110,6 +112,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  # Active storage - S3
-  config.active_storage.service = :amazon
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "http://www.kitessifieds.com" }
 end
